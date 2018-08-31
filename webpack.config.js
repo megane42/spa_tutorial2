7cdc5259c0bcdee63f646e4f319a4ad07eea77a5
megane42 <h.kzm.gm@gmail.com>
2018-08-31 18:54:42 +0900

    refine directories

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, '/')
+    path: path.join(__dirname, '/dist')
   },
   module: {
     rules: [
